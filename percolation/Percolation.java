f06a2d0dc238739d499deebce15dae0b246b7180
Ricardo <ricardoplouis@gmail.com>
2017-12-17 13:53:57 -0600

    Begun experimented with WeightedQuickUnionUF

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -1,3 +1,44 @@
+/******************************************************************************
+ *  Compilation:  javac Percolation.java
+ *  Execution:    java Percolation
+ *  Dependencies: WeightedQuickUnionUF.java
+ *
+ *  This program implements the WeightedQuickUnionUF class
+ *  into an api that is used for PercolationVisualizer and
+ *  InteractivePecolationVisualizer
+ *
+ *
+ ******************************************************************************/
+
+/**
+ * WeightedQuickUnionUF(int n)
+ * Initializes an empty union–find data structure with n sites 0 through n-1.
+ */
+
+
+/**
+ * WeightedQuickUnionUF methods
+ *
+ *
+ *
+ * boolean connected(int p, int q)
+ * Returns true if the the two sites are in the same component.
+ *
+ * int	count()
+ * Returns the number of components.
+ *
+ * int	find(int p)
+ * Returns the component identifier for the component containing site p.
+ *
+ * static void	main(String[] args)
+ * Reads in a sequence of pairs of integers (between 0 and n-1) from standard input, where each integer represents some object; if the sites are in different components, merge the two components and print the pair to standard output.
+ *
+ * void	union(int p, int q)
+ * Merges the component containing site p with the the component containing site q.
+ */
+
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
 
     /**
@@ -12,16 +53,19 @@ public class Percolation {
 
     private int array;
 
+
     Percolation(int n){
         array = n;
     }
+
+    WeightedQuickUnionUF uf = new WeightedQuickUnionUF(array);
 
     boolean open(int a, int b){
         return true;
     }
 
     boolean isOpen(int c, int d){
-        return true;
+        return uf.connected(c, d);
     }
 
     boolean percolates(){
