6cb19a34c33e3a6c890923359223e847decf7fdd
Ricardo <ricardoplouis@gmail.com>
2017-12-17 13:37:13 -0600

    First commit of algo course

---
+++ percolation/Percolation.java
@@ -0,0 +1,34 @@
+public class Percolation {
+
+    /**
+     * Needs the following:
+     *    Constructor(n)
+     *    open(int, int)
+     *    isOpen(int, int) return boolean
+     *    isFull(int, int) return boolean
+     *    percolates() return boolean
+     *
+     */
+
+    private int array;
+
+    Percolation(int n){
+        array = n;
+    }
+
+    boolean open(int a, int b){
+        return true;
+    }
+
+    boolean isOpen(int c, int d){
+        return true;
+    }
+
+    boolean percolates(){
+        return true;
+    }
+
+    boolean isFull(int k, int j){
+        return true;
+    }
+}
